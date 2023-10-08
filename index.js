4a4706a110045237f4ac13d23bfafc3f871cab40
Nyagol <nyagolpeter@gmail.com>
2023-10-08 16:22:38 +0300

    done and completed well and with ease

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+var customerName = 'bob'  
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob';
+   console.log(bestCustomer)
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+  console.log(bestCustomer)
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';
+}
+
+
